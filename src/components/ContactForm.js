b1ff2a160937aa236364bd41a4f47a721d0f7ba7
lenakernytska <yaapril222@gmail.com>
2021-03-25 17:57:22 +0200

    Changes Input

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -29,21 +29,27 @@ state = {
             <form className={styles.form} onSubmit={this.handleSubmit}>
         <label className={styles.label}>
                     Name
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            value={name}
-            autoComplete="off"
-            onChange={this.handleChange}
-          />
+         <input
+                className={styles.input}
+                type="text"
+                name="name"
+                value={name}
+                autoComplete="off"
+                onChange={this.handleChange}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                required
+/>
                 </label>
                  <label className={styles.label}>
                     Number
           <input
             className={styles.input}
-            type="number"
+            type="tel"
             name="number"
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+            required
             value={number}
             autoComplete="off"
             onChange={this.handleChange}
